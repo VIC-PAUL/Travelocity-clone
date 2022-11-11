9ad0a465c34c42efd80ebe85a8e2630906f04dc0
Victor Paul Choudhury <107463059+XKOBX@users.noreply.github.com>
2022-11-11 22:24:57 +0530

    files

--- tripify/src/index.js
+++ tripify/src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
-  
+  <BrowserRouter>
+    <ChakraProvider>
+       
+          <App />
+      
+    </ChakraProvider>
+  </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
